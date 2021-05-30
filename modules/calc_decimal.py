954b7f0b7bbd84e037821e1520590fcae957aff1
Rick Toews <rick.toews@gmail.com>
2021-05-30 14:22:35 -0700

    Convert values to non-decimal digits if a base greater than 10 is used. (Currently, just up to base 16 is supported.)

--- modules/calc_decimal.py
+++ modules/calc_decimal.py
@@ -1,6 +1,12 @@
 import math
 from modules.non_repeating import tally
 
+letter_digit = ['A', 'B', 'C', 'D', 'E', 'F']
+def get_digit(value):
+	if value > 9:
+		value = letter_digit[value-10]
+	return value
+
 def calc_decimal(reduced_num, reduced_denom, base):
 	section_list = ['', '', '']
 	section_ndx = 0
@@ -23,21 +29,17 @@ def calc_decimal(reduced_num, reduced_denom, base):
 	# BEGIN LONG DIVISION -----
 	while step_remainder != 0 and not step_remainder in remainder_flag_list:
 		remainder_flag_list.append(step_remainder)
-		step_digit = math.floor(step_remainder * base / reduced_denom)
+		step_value = math.floor(step_remainder * base / reduced_denom)
+		step_digit = get_digit(step_value)
 
 		if decimal_length == non_repeating_tally:
 			section_ndx += 1
 			# Store this digit; it'll be used to determine when/if the complement begins.
 			start_repeat = step_remainder
 
-#		if base != 10:
-# Not sure what base_convert is. Do we need to write something in Python?
-#			section_list[section_ndx] += base_convert(step_digit, 10, base)
-#		else:
-
 		section_list[section_ndx] += str(step_digit)
 
-		step_remainder = step_remainder * base - step_digit * reduced_denom
+		step_remainder = step_remainder * base - step_value * reduced_denom
 		if step_remainder + start_repeat == reduced_denom:
 			section_ndx += 1
 
